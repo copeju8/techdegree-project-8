aec7ffcc95e55dbea75afb00ea8474c4fa8ef5ab
copeju8 <51244110+copeju8@users.noreply.github.com>
2019-08-25 23:57:41 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,107 +1,84 @@
-//Create and retrieve data from db
+//Entry point - create and retrieve data from db
 
-'use strict';
-const Sequelize = require('sequelize');
+const app = require('./app');
+const debug = require('debug')('express-sequelize');
+const http = require('http');
+const models = require('/db/models');
 
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+  /**
+   * Create HTTP server.
+   */
+const server = http.createServer(app);
 
-    (async () => {
-        await db.sequelize.sync({ force: true });
-
-        try {
-            const books = await Books.create ({  })
-
-
-        } catch (error)  {
-          if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(err => err.message);
-            console.error('Validation errors: ', errors);
-          } else {
-            throw error;
-          }   
-        }
-    }) ();
-
-    const express = require('express');     //Access all the methods and properties of the Express Module
-    const bodyParser = require('body-parser');
-    
-    const app = express();                  //Create an express application that returns an express application - extend this app using (routes/middleware)
-    
-    'use strict';                           //The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
-                                            //With strict mode, you can not, for example, use undeclared variables.
-    
-    let jsonData = require('./Data/data.json');                    
-    let projects = jsonData.projects
-    
-    app.use(bodyParser.urlencoded({ extended: false}));     //Body-parser turns string into usable data for our application/ call the body 
-    
-    app.set('view engine', 'pug');              //Tells express which template to use - the pug template found in directory views
-    
-                                                //Getting routes, loading the content (e.g. index page) - asking to view or get a webpage (the URL) 
-    app.get('/', (req, res) => {                //Takes callback with two parameters.
-        res.render('index', { projects });      //Render's the visitor's route and initiates the pug file/JavaScript template language from the view directory
-    });  
-    
-    
-    app.use('/static', express.static('public'));       //Requires a middleware- handles data and hands it off
-    app.post('/index', (req, res) => {   
-        res.render('index');
-    });  
-    
-                                                //Getting routes, loading the content (e.g. about page) - asking to view or get a webpage (the URL) 
-    app.get('/about', (req, res) => {   
-        res.render('about');
-    });  
-    app.use('/static', express.static('public'));       //Requires a middleware- handles data and hands it off
-    app.post('/about', (req, res) => {   
-        res.render('about');
-    });  
-    
-                                                    //Getting routes, loading the content (e.g. individual page) - asking to view or get a webpage (the URL) 
-    app.get('/projects/:id', (req, res) => { 
-        const{id}= req.params
-        const project = projects[id]  
-        res.render('project', {project} );
-    });  
-    
-    app.use('/projects', express.static('public'));       //Requires a middleware- handles data and hands it off
-    app.post('/projects', (req, res) => {   
-        res.render('/project');
-    });  
-    
-    //Handling Errors from get requests
-    app.use((req, res, next) => {
-        const error = new Error('Sorry, your request is not working!');
-        error.status = 404
-        next(error);
+models.sequelize.sync().then(function() {
+    /**
+     * Listen on provided port, on all network interfaces.
+     */
+    server.listen(port, function() {
+      debug('Express server listening on port ' + server.address().port);
     });
-    
-    app.use((error, req, res, next) => {
-        res.locals.error = error;
-        res.status(error.status);
-        res.render('error');
-    });
-    
-    app.listen(3000, () =>  {                                               //Set up developer server using the listening method with port - an express app
-        console.log('The application is running on localhost:3000!')        //Send request through local host
-    })
-    
-    
-
-
-
-
-
-
-   //The associate() method is called in the db/index.js file after each model is imported into the Sequelize instance. 
-   //This allows code within the associate() method to access any of the available models.
-
-    Books.associate = (models) =>  {
-       Books.hasMany(models.Books);
-    };
-
-
-app.set('view engine', 'pug')
-
-app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
-  })
+    server.on('error', onError);
+    server.on('listening', onListening);
+  });
+  
+  /**
+   * Normalize a port into a number, string, or false.
+   */
+  
+  function normalizePort(val) {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+  
+  /**
+   * Event listener for HTTP server "error" event.
+   */
+  
+  function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    const bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+  
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  
+  function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  }

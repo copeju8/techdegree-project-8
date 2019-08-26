4b0880f92f741c876cb4ff2c9c781779e4c33815
copeju8 <51244110+copeju8@users.noreply.github.com>
2019-08-25 23:59:01 -0400

    Update index

--- models/index.js
+++ models/index.js
@@ -1,25 +1,38 @@
 //Configures the Sequelize ORM
 
-const options = {
-    dialect: 'sqlite',
-    storage: 'library.db',  //Set the file path or storage engine
-    define: {
-        timestamps: false,
-    },
-};
+'use strict';
 
-//Loading books.js
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(__filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.js')[env];
+var db        = {};
 
-const db = {
-    sequelize,
-    Sequelize,
-    models: {},
-};
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-//import new model and 
-//The file exports the db object, which holds the Sequelize and database configurations, as well as all the models.
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
-db.models.Books = require('./models/books.js')(sequelize);
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
-
